894bbfd246d058c921db80c056d8bf4843e14cce
thmsrbrt <thomas.robert1@icloud.com>
2023-09-18 12:23:43 +0200

    calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def divide(self, a, b):
+        if a != 0:
+            return a / b
+
