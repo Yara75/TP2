7d8a30505cbeaffaa959a6407974c6bef4121413
thmsrbrt <thomas.robert1@icloud.com>
2023-09-18 12:41:25 +0200

    fix self

--- test_calculator.py
+++ test_calculator.py
@@ -1,16 +1,21 @@
 import pytest
 from calculator import Calculator
 
-def test_add():
-    assert Calculator.add(1,1) == 2
-    assert Calculator.add(-1,1) == 0
 
-def test_add2():
-    assert Calculator.add(10,10) == 20
+class TestCalculator:
 
+    def __init__(self):
+        self.cal = Calculator()
 
+    def test_add(self):
+        assert Calculator.add(self.cal, 1, 1) == 2
+        assert Calculator.add(self.cal, -1, 1) == 0
 
+    def test_add2(self):
+        assert Calculator.add(self.cal, 10, 10) == 20
 
-
-
-
+    def test_substract(self):
+        assert Calculator.subtract(self.cal, 10, -1) == 9
+        assert Calculator.subtract(self.cal, -10, -1) == -11
+        assert Calculator.subtract(self.cal, 0, -0) == 0
+        assert Calculator.subtract(self.cal, "ze", 1) == Exception
